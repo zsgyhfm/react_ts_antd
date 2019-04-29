5df0812af072fb553184f4dd5b0f9f31fbc5ce49
hhh <hhh@gmail.com>
2019-04-29 14:00:00 +0800

    使用react-loadable 懒加载组件

--- src/pages/Whois.tsx
+++ src/pages/Whois.tsx
@@ -1,8 +1,19 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
-import Header from '../componets/Headermi'
 import DocumentTitle from 'react-document-title'
-import Mi from "../componets/Mi"
+import loadable from 'react-loadable'
+// import Mi from "../componets/Mi"
+//懒加载Mi 组件 这种类型不一致导致无法取得children,tsx下不可取， 改用react-loadable 
+// const Mi=lazy(()=>import('../componets/Mi'))
+
+
+// 使用loadeable 来懒加载
+const Mi = loadable({
+    loader:()=>import("../componets/Mi"),
+    loading(){
+        return <div>loading</div>
+    }
+})
 class Whois extends React.Component {
     render() {
         return (
